0e92d531b2900898af8a431a8513eb6e429f18b9
rodrigocitadin <citadinrodrigo@gmail.com>
2023-02-27 21:59:22 -0300

    idk

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  <header class="bg-amber-100">
+    <h1>
+      oi
+    </h1>
   </header>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  content: ["./src/**/*.{html,js,vue}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
