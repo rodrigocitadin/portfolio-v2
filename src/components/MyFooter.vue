c98d56b92259d5437055131b950f18771f3db865
rodrigocitadin <citadinrodrigo@gmail.com>
2023-03-04 16:44:49 -0300

    rollback because a lot of issues 2.0

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="mt-24 pb-2 text-sm">
-    <h3 class="text-center">
+  <div class="max-w-3xl mx-auto mt-32 pb-2 text-sm">
+    <h3 class="ml-8">
       Copyright © 2023 Citadin
     </h3>
   </div>
